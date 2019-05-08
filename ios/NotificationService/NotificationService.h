db61da44097693f2c1f8c7c3c17f42e490cae5c6
Vladimir Upirov <vladimir.upirov@themidnightcoders.com>
2019-05-08 10:59:37 +0200

    - advanced setup for ios

---
+++ ios/NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by Valodya Upirov on 5/7/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
---
+++ ios/NotificationService/NotificationService.m
@@ -0,0 +1,34 @@
+//
+//  NotificationService.m
+//  NotificationService
+//
+//  Created by Valodya Upirov on 5/7/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "NotificationService.h"
+#import <RNBackendless/RNBackendless.h>
+
+@interface NotificationService ()
+
+@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
+@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+
+@end
+
+@implementation NotificationService
+
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+  self.contentHandler = contentHandler;
+  self.bestAttemptContent = [request.content mutableCopy];
+  
+  [RNBackendless processMutableContent:request withContentHandler:contentHandler];
+}
+
+- (void)serviceExtensionTimeWillExpire {
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    self.contentHandler(self.bestAttemptContent);
+}
+
+@end
